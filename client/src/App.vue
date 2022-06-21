c0a8f2d06dd994cb7088362f7f1e4a698e88eae8
Dejan Mirkovic <dejanmirkovic01@gmail.com>
2022-06-21 18:19:28 +0200

    21.6.2022

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <PageHeader />
+    
+
+     <router-view></router-view>
+   
   </div>
 </template>
 
 <script>
+import PageHeader from '@/components/Header'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+   PageHeader
+  }
 }
 </script>
 
@@ -18,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+<v-card
+
+    color="grey lighten-4"
+    flat
+    
+    tile
+    
+  >
+    <v-toolbar  dense>
+        
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        <span class="home" @click="navigateTo({name: 'root'})">
+        Tab Tracker
+        </span>
+        </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+        
+        
+        
+        
+        <!-- <v-toolbar-items>
+      <v-btn >
+        Browse
+      </v-btn>
+        </v-toolbar-items> -->
+
+        <v-toolbar-items>
+        
+      <v-btn @click="navigateTo({name: 'Register'})">
+         Sign Up
+      </v-btn>
+    
+        </v-toolbar-items>
+
+<v-toolbar-items>
+      <v-btn @click="navigateTo({name: 'Login'})">
+        Login
+      </v-btn>
+</v-toolbar-items>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+ 
+</template>
+
+<script>
+export default {
+methods:{
+    navigateTo(route){
+this.$router.push(route)
+    }
+}
+}
+</script>
+
+<style scoped>
+.home{
+    cursor: pointer;
+}
+.home:hover{
+    color: darkslateblueW;
+}
+.re{
+    color:red
+}
+
+</style>
